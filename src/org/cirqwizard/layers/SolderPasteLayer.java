018437c06f9cc73ef84158304a8d8c9d2c631b8b
Simon <simon.salykov@gmail.com>
2014-03-05 14:43:38 +0100

    CQ-63 Implemented solder paste dispensing tool paths generation for regions

--- src/org/cirqwizard/layers/SolderPasteLayer.java
+++ src/org/cirqwizard/layers/SolderPasteLayer.java
@@ -15,15 +15,18 @@ This program is free software: you can redistribute it and/or modify
 package org.cirqwizard.layers;
 
 import org.cirqwizard.appertures.RectangularAperture;
+import org.cirqwizard.geom.Line;
 import org.cirqwizard.geom.Point;
 import org.cirqwizard.gerber.Flash;
 import org.cirqwizard.gerber.GerberPrimitive;
+import org.cirqwizard.gerber.LinearShape;
+import org.cirqwizard.gerber.Region;
 import org.cirqwizard.logging.LoggerFactory;
-import org.cirqwizard.math.RealNumber;
+import org.cirqwizard.math.MathUtil;
 import org.cirqwizard.toolpath.LinearToolpath;
 import org.cirqwizard.toolpath.Toolpath;
 
-import java.util.*;
+import java.util.ArrayList;
 import java.util.logging.Level;
 
 
@@ -75,6 +78,26 @@ public class SolderPasteLayer extends Layer
         return min;
     }
 
+
+    private void fillRectangle(ArrayList<Toolpath> toolpaths, Point from, Point to, int width, int needleDiameter)
+    {
+        int passes = Math.max(1, width / (needleDiameter * 2));
+        for (int i = 0; i < passes; i++)
+        {
+            // Shortening tool path for needle radius
+            double angle = new Line(from, to).angleToX();
+            from = from.add(new Point((int) (Math.cos(angle) * needleDiameter / 2), (int) (Math.sin(angle) * needleDiameter / 2)));
+            to = to.subtract(new Point((int) (Math.cos(angle) * needleDiameter / 2), (int) (Math.sin(angle) * needleDiameter / 2)));
+
+            // Offsetting
+            angle = MathUtil.bindAngle(angle - Math.PI / 2);
+            int offset = width / (passes + 1) * (i + 1);
+            Point offsetVector = new Point((int)(Math.cos(angle) * offset), (int)(Math.sin(angle) * offset));
+            to.add(offsetVector);
+            toolpaths.add(new LinearToolpath(needleDiameter, from.add(offsetVector), to.add(offsetVector)));
+        }
+    }
+
     public void generateToolpaths(int needleDiameter)
     {
         toolpaths = new ArrayList<>();
@@ -111,11 +134,49 @@ public class SolderPasteLayer extends Layer
                 else
                     System.out.println("Circular apertures not supported at the moment");
             }
+            else if (element instanceof Region)
+            {
+                Region region = (Region) element;
+                LinearShape longestSide = null;
+                for (GerberPrimitive p : region.getSegments())
+                {
+                    if (p instanceof LinearShape)
+                    {
+                        if (longestSide == null ||
+                                longestSide.getFrom().distanceTo(longestSide.getTo()) < ((LinearShape) p).getFrom().distanceTo(((LinearShape) p).getTo()))
+                            longestSide = (LinearShape) p;
+                    }
+                }
+
+                double largestWidth = 0;
+                for (GerberPrimitive p : region.getSegments())
+                {
+                    if (p instanceof LinearShape)
+                    {
+                        double d = calculatePerpendicular(longestSide.getFrom(), longestSide.getTo(), ((LinearShape) p).getFrom());
+                        if (d > largestWidth)
+                            largestWidth = d;
+                        d = calculatePerpendicular(longestSide.getFrom(), longestSide.getTo(), ((LinearShape) p).getTo());
+                        if (d > largestWidth)
+                            largestWidth = d;
+                    }
+                }
+
+                fillRectangle(toolpaths, longestSide.getFrom(), longestSide.getTo(), (int)largestWidth, needleDiameter);
+            }
             else
             {
                 LoggerFactory.getApplicationLogger().log(Level.WARNING, "Unexpected element on solder paste level: " + element);
             }
         }
+    }
+
+    private double calculatePerpendicular(Point from, Point to, Point p)
+    {
+        double dx = from.getX() - to.getX();
+        double dy = from.getY() - to.getY();
+
+        return Math.abs(dy * p.getX() - dx * p.getY() + from.getX() * to.getY() - to.getX() * from.getY()) / Math.sqrt(dx * dx + dy * dy);
     }
 
     @Override
