2b96e7dc098b55bd7797eccf18372cf2fbf15689
Simon <simon.salykov@gmail.com>
2014-08-06 00:14:29 +0200

    CQ-116 File formatting fixed

--- src/org/cirqwizard/settings/ApplicationConstants.java
+++ src/org/cirqwizard/settings/ApplicationConstants.java
@@ -1,8 +1,19 @@
+/*
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.cirqwizard.settings;
 
-/**
- * Created by simon on 05/08/14.
- */
 public class ApplicationConstants
 {
     public static final int RESOLUTION = 1000;
--- src/org/cirqwizard/settings/SettingsGroup.java
+++ src/org/cirqwizard/settings/SettingsGroup.java
@@ -1,3 +1,17 @@
+/*
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.cirqwizard.settings;
 
 import org.cirqwizard.logging.LoggerFactory;
@@ -10,9 +24,6 @@ import java.lang.reflect.ParameterizedType;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
-/**
- * Created by simon on 04/08/14.
- */
 public abstract class SettingsGroup
 {
     public abstract String getName();
